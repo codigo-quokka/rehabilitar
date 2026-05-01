3a1d0175395e841de307e6437128bf931a513330
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-01 13:57:02 -0300

    feat(auth): add Auth endpoint

--- Rehabilitar/src/API/Program.cs
+++ Rehabilitar/src/API/Program.cs
@@ -1,6 +1,10 @@
+using Infrastructure;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,10 +16,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
