f0526a7aa19084af0e4be1243ea0413d9911c7c2
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-03 20:04:49 -0300

    feat(auth): implement email verification process and enhance login flow

--- Rehabilitar-back/src/Application/Auth/DTOs/VerifyEmailRequest.cs
+++ Rehabilitar-back/src/Application/Auth/DTOs/VerifyEmailRequest.cs
@@ -3,11 +3,11 @@ namespace Application.Auth.DTOs;
 public class VerifyEmailRequest
 {
     public string UserId {get; private set;}
-    public string Token {get; private set;}
+    public string ConfirmationToken {get; private set;}
 
-    public VerifyEmailRequest(string userId, string token)
+    public VerifyEmailRequest(string userId, string confirmationToken)
     {
         UserId = userId;
-        Token = token;
+        ConfirmationToken = confirmationToken;
     }
 }
