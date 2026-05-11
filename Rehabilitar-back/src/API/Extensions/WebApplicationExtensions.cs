58ae749197bda51d32aa756649c210ca1ce461f6
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-11 10:14:29 -0300

    refactor(seeding): implement extension method and separate class for role seeding

---
+++ Rehabilitar-back/src/API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,20 @@
+using Infrastructure.Persistence.Seeding;
+
+namespace API.Extensions;
+
+public static class WebApplicationExtensions
+{
+    // método de extensión, extiende el tipo WebApplication (app en Program.cs) y le agrega este método para usar el seeding.
+    public static async Task UseIdentitySeedingAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        try
+        {
+            await IdentitySeeder.SeedRolesAsync(scope.ServiceProvider);
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine("Error al seedear roles: " + e.Message);
+        }
+    }
+}
