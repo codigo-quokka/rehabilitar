d9d07a687ffac91697f5aee7e45807e7e6b4bb48
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-13 19:39:52 -0300

    feat(Sala): add unique index on Nombre column in Salas table and update DbContext configuration

---
+++ Rehabilitar-back/src/Infrastructure/Persistence/Migrations/20260513223526_UniqueSalaNombre.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueSalaNombre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Salas_Nombre",
+                table: "Salas",
+                column: "Nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Salas_Nombre",
+                table: "Salas");
+        }
+    }
+}
