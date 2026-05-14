fe3a03638a8491f019edf8f8d6a403082202a11a
Pelo <ignaccio98_hassan@hotmail.com>
2026-05-14 05:28:44 -0300

    fix(Actividades): use JsonStringEnumConverter and update DTOs

--- Rehabilitar-back/src/API/Program.cs
+++ Rehabilitar-back/src/API/Program.cs
@@ -9,7 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
-builder.Services.AddControllers();
+ builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Esto hace que los Enums se manden como Strings en el JSON de salida
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
