af52f663b0b2e90e4ac7c588ce8b167b4d0fe51e
Holger Bermeo <bermeoholger@gmail.com>
2026-05-16 13:08:12 -0300

    fix: resolve merge conflicts in DependencyInjection and UsuariosPage

--- Rehabilitar-back/src/Application/Common/DependencyInjection.cs
+++ Rehabilitar-back/src/Application/Common/DependencyInjection.cs
@@ -14,11 +14,8 @@ public static class DependencyInjection
         services.AddScoped<ISalaService, SalaService>();
         services.AddScoped<IActividadService, ActividadService>();
         services.AddScoped<IAuthService, AuthService>();
-<<<<<<< HEAD
         services.AddScoped<IProfesorService, ProfesorService>();
-=======
         services.AddScoped<IUsuarioService, UsuarioService>();
->>>>>>> e3fed1edccecfcf311af1a4e143638ddcc847574
 
         return services;
     }
