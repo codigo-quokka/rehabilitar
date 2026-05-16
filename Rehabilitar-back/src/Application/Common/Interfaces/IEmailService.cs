8c9b289a7429feb65587fe839e314bd8f9b4f5b1
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-16 20:19:00 -0300

    feat(reset-password): add password reset email functionality

--- Rehabilitar-back/src/Application/Common/Interfaces/IEmailService.cs
+++ Rehabilitar-back/src/Application/Common/Interfaces/IEmailService.cs
@@ -5,4 +5,5 @@ namespace Application.Common.Interfaces;
 public interface IEmailService
 {
     Task<ErrorOr<Success>> SendConfirmationEmail(string userEmail, string verificationLink);
+    Task<ErrorOr<Success>> SendPasswordResetEmail(string userEmail, string verificationLink);
 }
