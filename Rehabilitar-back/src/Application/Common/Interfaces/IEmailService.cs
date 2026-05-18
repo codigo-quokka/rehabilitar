0d7c822df2d80aacedf8dfe045baec8ba626e089
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-18 18:05:10 -0300

    feat(email): add SendNewUserWithCredentialsEmail method to send user credentials

--- Rehabilitar-back/src/Application/Common/Interfaces/IEmailService.cs
+++ Rehabilitar-back/src/Application/Common/Interfaces/IEmailService.cs
@@ -6,4 +6,5 @@ public interface IEmailService
 {
     Task<ErrorOr<Success>> SendConfirmationEmail(string userEmail, string verificationLink);
     Task<ErrorOr<Success>> SendPasswordResetEmail(string userEmail, string verificationLink);
+    Task<ErrorOr<Success>> SendNewUserWithCredentialsEmail(string userEmail, string password);
 }
