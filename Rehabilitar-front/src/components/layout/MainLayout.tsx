cdae73f9e63fab552dc0c121051c41a3f1ec5e4e
Breiki2 <santibreasb41@gmail.com>
2026-05-18 17:02:37 -0300

    chore: detallles visuales

--- Rehabilitar-front/src/components/layout/MainLayout.tsx
+++ Rehabilitar-front/src/components/layout/MainLayout.tsx
@@ -9,7 +9,7 @@ interface MainLayoutProps {
 
 export function MainLayout({ children, title }: MainLayoutProps) {
   return (
-    <div className="min-h-screen bg-linear-to-br from-dark-green/10 from-10% via-dark-green/30 via-50% to-dark-green/40 to-70% dark:from-0% dark:via-45% dark:to-90% dark:from-gray-700 dark:via-gray-950 dark:to-gray-900 bg-fixed flex flex-col">
+    <div className="min-h-screen bg-linear-to-br from-primary/10 from-10% via-primary/30 via-50% to-primary/40 to-70% dark:from-0% dark:via-45% dark:to-90% dark:from-gray-700 dark:via-gray-950 dark:to-gray-900 bg-fixed flex flex-col">
       <Header title={title} />
       <Sidebar />
       <main id="main-content" className="flex-1 p-8" tabIndex={-1}>
