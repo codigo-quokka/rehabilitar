6151514b983a4cc1ec070bdbf2fe0b849aa476b3
Ignacio Ahmed Hassan <76469238+IAhmedHassan@users.noreply.github.com>
2026-05-18 21:23:36 +0000

    fix(validacion actividad): chequear si la actividad superpuesta no tinee la misma serieId

--- Rehabilitar-back/src/Application/Actividades/IActividadRepository.cs
+++ Rehabilitar-back/src/Application/Actividades/IActividadRepository.cs
@@ -9,6 +9,6 @@ public interface IActividadRepository : IRepositoryBase<Actividad>
     Task<Actividad> ObtenerPorIdAsync(Guid actividadId, CancellationToken ct = default);
     Task<ICollection<Actividad>> ListarPorSerieIdAsync(Guid serieId, CancellationToken ct = default);
     Task<ICollection<Actividad>> ListarPorProfesorIdAsync(Guid profesorId, CancellationToken ct = default);
-    Task<bool> ExisteActividadSuperpuestaEnSalaAsync(Guid salaId, DateTime fechaYHora, Guid? actividadId, CancellationToken ct = default);
-    Task<bool> ExisteActividadSuperpuestaEnProfesorAsync(Guid profesorId, DateTime fechaYHora, Guid? actividadId, CancellationToken ct = default);
+    Task<bool> ExisteActividadSuperpuestaEnSalaAsync(Guid salaId, DateTime fechaYHora, Guid? actividadId, Guid serieId, CancellationToken ct = default);
+    Task<bool> ExisteActividadSuperpuestaEnProfesorAsync(Guid profesorId, DateTime fechaYHora, Guid? actividadId, Guid serieId, CancellationToken ct = default);
 }
