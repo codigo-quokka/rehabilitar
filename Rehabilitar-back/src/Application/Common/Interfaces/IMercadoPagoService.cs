381a63e44c1ae470aad26f59d2b4ddb863c96d86
Pelo <ignacio98_hassan@hotmail.com>
2026-05-24 20:07:59 -0300

    refactor(infrastructure): generalizar referencia externa en MercadoPagoService

--- Rehabilitar-back/src/Application/Common/Interfaces/IMercadoPagoService.cs
+++ Rehabilitar-back/src/Application/Common/Interfaces/IMercadoPagoService.cs
@@ -4,6 +4,6 @@ namespace Application.Common.Interfaces;
 
 public interface IMercadoPagoService
 {
-    Task<ErrorOr<(string PreferenceId, string InitPoint)>> CreatePreferenceAsync(string reservaId, decimal amount, string description);
-    Task<ErrorOr<(bool IsApproved, string ReservaId)>> GetPaymentStatusAsync(string paymentId);
+    Task<ErrorOr<(string PreferenceId, string InitPoint)>> CreatePreferenceAsync(string externalReference, decimal amount, string description);
+    Task<ErrorOr<(bool IsApproved, string ExternalReference)>> GetPaymentStatusAsync(string paymentId);
 }
