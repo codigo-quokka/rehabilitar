b0f8fb80aa0d61185ad737d20b8efbf53a28aa69
pelo <ignacio98_hassan@hotmail.com>
2026-05-26 15:16:24 -0300

    chore(EmailService): añadir tipos de correo a implementar

--- Rehabilitar-back/src/Infrastructure/Email/EmailService.cs
+++ Rehabilitar-back/src/Infrastructure/Email/EmailService.cs
@@ -112,6 +112,84 @@ public class EmailService : IEmailService
         return await SendEmailAsync(userEmail, "Apto físico rechazado", content);
     }
 
+    public async Task<ErrorOr<Success>> SendReservaRealizadaEmail(string userEmail, string link)
+    {
+        // cuando se hizo el pago. debería informar si está en lista de espera o no
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendReservaCanceladaEmail(string userEmail, string link)
+    {
+        // cuando se cancela la reserva. Debería informar si se hizo un reembolso (en plata o rehabiliCoin) o no,
+        // y por qué no en caso de que no se lo haya hecho (ej cancelación dentro de las 24hs). Además 
+        // debería informar del descuento que le pertenece dependiendo de las cancelaciones consecutivas que tenga
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendCupoLiberadoEmail(string userEmail, string link)
+    {
+        // Cuando se libera un cupo en una actividad, informa a los clientes en la lista de espera 
+        // que se liberó un cupo (debería mandarse todas las veces? o sólo al cliente que entró por el cupo liberado? 
+        // si son las dos debería hacerse un SendReservaConfirmadaEmail)
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendSuscripcionRealizadaEmail(string userEmail, string link)
+    {
+        // cuando el cliente realiza una suscripción. Debería informar las reservas que se realizaron, 
+        // incluyendo para cada una si quedó activa o en lista de espera
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendRecordatorioSuscripcionEmail(string userEmail, string link)
+    {
+        // No es tan importante. Si el cliente tiene una suscripción activa y se vence el mes, 
+        // se podría mandar un recordatorio para renovar la suscripción 
+        // (también podría mutar a un SendSuscripcionCanceladaEmail)
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendActividadCanceladaEmail(string userEmail, string link)
+    {
+        // Cuando se cancela una actividad. Debería informar a clientes y profesores que se canceló 
+        // la actividad (y el motivo?), e informar además a los clientes de su respectivo reembolso
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendDictadoPendienteEmail(string userEmail, string link)
+    {
+        // Cuando hay una actividad con dictado pendiente, se informa a los profesores aptos 
+        // para dictarlos (según el tren) que está dispo pa dictarla
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendRecordatorioActividadProximaEmail(string userEmail, string link)
+    {
+        // Cuando faltan 24 horas para que empiece una actividad, debería informar a clientes con reservas 
+        // realizadas que se vienen cositas (2do Sprint)
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendCagadaAPedoPorAusenciaEmail(string userEmail, string link)
+    {
+        // Cuando termina una actividad, debería informar a los clientes que no pasaron el presente 
+        // cuantas ausencias consecutivas tiene y las penalizaciones correspondientes si llega a 3
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendCuentaSuspendidaEmail(string userEmail, string link)
+    {
+        // Cuando se suspende una cuenta. Debería informar la razón de la suspensión 
+        // y los pasos a seguir para reactivarla
+        throw new NotImplementedException();
+    }
+
+    public async Task<ErrorOr<Success>> SendCuentaReactivadaEmail(string userEmail, string link)
+    {
+        // Cuando se reactiva una cuenta. Debería informar al cliente que se porte bien
+        throw new NotImplementedException();
+    }
+
     // --- MÉTODOS PRIVADOS DE INFRAESTRUCTURA ---
 
     private async Task<ErrorOr<Success>> SendEmailAsync(string userEmail, string subject, string htmlContent)
