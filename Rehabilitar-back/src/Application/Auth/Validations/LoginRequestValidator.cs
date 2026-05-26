a639e57aad845f07c885fdd7c6a3a71353f5fca6
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-26 13:27:23 -0300

    refactor(back): improve validation messages for auth requests

--- Rehabilitar-back/src/Application/Auth/Validations/LoginRequestValidator.cs
+++ Rehabilitar-back/src/Application/Auth/Validations/LoginRequestValidator.cs
@@ -7,7 +7,7 @@ public class LoginRequestValidator : AbstractValidator<LoginRequest>
 {
     public LoginRequestValidator()
     {
-        RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("El email no es válido.");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("La contraseña es requerida.");
     }
 }
