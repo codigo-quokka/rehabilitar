841910c637ae5c39b33a05e4cce851ff1806430b
Gonzalo <111930186+geonnn@users.noreply.github.com>
2026-05-31 01:40:01 -0300

    refactor(domain): move client personal info to User entity

--- Rehabilitar-back/src/Domain/User.cs
+++ Rehabilitar-back/src/Domain/User.cs
@@ -1,3 +1,5 @@
+using Domain.Clientes;
+using Domain.Exceptions;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain;
@@ -6,32 +8,38 @@ public class User : IdentityUser<Guid>
 {
     public string FirstName {get; private set;}
     public string LastName {get; private set;}
+    public Dni Dni {get; private set;}
+    public DateOnly FechaNacimiento { get; private set; }
 
     // constructor vacío para EF Core.
     #nullable disable
     private User() { }
     #nullable enable
 
-    private User(string firstName, string lastName, string email)
+    private User(string firstName, string lastName, string email, Dni dni, DateOnly fechaNacimiento, string? phoneNumber = null)
     {
         Id = Guid.NewGuid();
         FirstName = firstName;
         LastName = lastName;
         UserName = email;
         Email = email;
+        PhoneNumber = phoneNumber;
+        Dni = dni;
+        FechaNacimiento = fechaNacimiento;
     }
 
-    public static User Create(string firstName, string lastName, string email)
+    public static User Create(string firstName, string lastName, string email, string dni, DateOnly fechaNacimiento, string? phoneNumber = null)
     {
-        return new User(firstName, lastName, email);
+        ValidarMayorDeEdad(fechaNacimiento);
+        Dni dniObj = new Dni(dni);
+        return new User(firstName, lastName, email, dniObj, fechaNacimiento, phoneNumber);
     }
 
-    public void UpdateInfo(string firstName, string lastName, string email)
+    public void UpdateInfo(string firstName, string lastName, string? phoneNumber = null)
     {
         FirstName = firstName;
         LastName = lastName;
-        UserName = email;
-        Email = email;
+        PhoneNumber = phoneNumber;
     }
 
     public void Suspender()
@@ -44,5 +52,14 @@ public class User : IdentityUser<Guid>
     {
         LockoutEnabled = false;
         LockoutEnd = null;
+    }
+
+    private static void ValidarMayorDeEdad(DateOnly fechaNac)
+    {
+        var hoy = DateOnly.FromDateTime(DateTime.Today);
+        if (fechaNac.AddYears(18) > hoy)
+        {
+            throw new DomainException("Debe ser mayor de edad para registrarse en el sitio.");
+        }
     }
 }
