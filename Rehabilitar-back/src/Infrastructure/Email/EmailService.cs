fc250e65ebf5d5c3b9c5b556bb99a11f22838584
Pelo <ignacio98_hassan@hotmail.com>
2026-05-31 19:10:52 -0300

    FEAT: borre un '{'

--- Rehabilitar-back/src/Infrastructure/Email/EmailService.cs
+++ Rehabilitar-back/src/Infrastructure/Email/EmailService.cs
@@ -222,6 +222,7 @@ public class EmailService : IEmailService
 
         return await SendEmailAsync(userEmail, "Cuenta reactivada", content);
     }
+    
 
     // --- MÉTODOS PRIVADOS DE INFRAESTRUCTURA ---
 
