3fa2dbd043924f37afe6a97a76a87013a914a85e
Pelo <ignacio98_hassan@hotmail.com>
2026-05-31 21:02:03 -0300

    feat(persistence): add migration for payment intent expiration date

--- Rehabilitar-back/src/Infrastructure/Persistence/Migrations/20260531234114_AddFechaExpiracionToIntencionPago.Designer.cs
+++ Rehabilitar-back/src/Infrastructure/Persistence/Migrations/20260531234114_AddFechaExpiracionToIntencionPago.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RehabilitarDbContext))]
-    partial class RehabilitarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260531234114_AddFechaExpiracionToIntencionPago")]
+    partial class AddFechaExpiracionToIntencionPago
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
@@ -140,15 +143,6 @@ namespace Infrastructure.Persistence.Migrations
 
                     b.Property<int>("CancelacionesConsecutivas")
                         .HasColumnType("INTEGER");
-
-                    b.Property<decimal>("DescuentoProximaReserva")
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Dni")
-                        .IsRequired()
-                        .HasMaxLength(8)
-                        .HasColumnType("TEXT")
-                        .HasColumnName("Dni");
 
                     b.Property<decimal>("DescuentoProximaReserva")
                         .HasColumnType("TEXT");
